5adf087d70d2a96721254d78e13ac4f1735b329e
MikaelAndriIngason <mikaelin258@nemi.tskoli.is>
2020-12-06 18:04:59 +0000

    Build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+/*module.exports = {
     publicPath: '/'
-}
+}*/
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/Utilegan/'
+      : '/'
+  }
